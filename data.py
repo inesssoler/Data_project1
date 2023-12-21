e09059a433c56ec617c0932d47786609f252955c
Luseca <148787198+Luseca@users.noreply.github.com>
2023-12-21 12:23:06 +0100

    Update data.py

--- data.py
+++ data.py
@@ -7,12 +7,13 @@ from sqlalchemy import create_engine, text
 import psycopg2
 import unicodedata
 
+# Adapta la librería 'Faker' al español castellano con nombres en dicho idioma
 fake = Faker('es_ES')
 
+# Cantidad de solicitantes a generar
 num_registros = 500
 
 # Tabla generada de las solicitudes
-
 base_de_datos = []
 
 # Crea los registros de forma individual
@@ -29,6 +30,7 @@ for _ in range(num_registros):
         'Indique si vive en una residencia de mayores': fake.boolean(),
         'Indique si viajará con acompañante': fake.boolean(),
         'Indique si el año pasado disfrutó de algún viaje': fake.boolean(),
+        'Indique si en 2021 disfrutó de algún viaje': fake.boolean(),
         'Importe de su pensión percibida': round(random.uniform(480,3000))
     }
     if datos['Indique si presenta alguna discapacidad'] == True:
@@ -42,10 +44,9 @@ for _ in range(num_registros):
 # Crea un DataFrame a partir de la lista
     df = pd.DataFrame(base_de_datos)
 
-# Guarda la base de datos en un JSON con codificación utf-8 y ensure_ascii=False, facilitando las tildes
+# Guarda la base de datos en un JSON con codificación utf-8 y ensure_ascii=False, facilitando las tildes e imprimiéndolo por pantalla
 with open('base_de_datos.json', 'w', encoding='utf-8') as archivo:
     json.dump(base_de_datos, archivo, indent=2, ensure_ascii=False)
 
 print("Base de datos generada y guardada en 'base_de_datos.json'")
-
 #------------------------------------------------------------------------------------------------------------------------------------------
