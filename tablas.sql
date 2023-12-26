b9d5fa4fa83cb2f936d8215eccdea2426e33d393
jarupu <javieruizpuchol@gmail.com>
2023-12-26 12:27:33 +0100

    primer intento

--- tablas.sql
+++ tablas.sql
@@ -5,90 +5,59 @@ CREATE SCHEMA IF NOT EXISTS esquema;
 
 -- Table: esquema."Solicitudes"
 
-CREATE TABLE esquema."Solicitudes"
+CREATE TABLE esquema.solicitudes
 (
-    index bigint,
-    "Nombre" text COLLATE pg_catalog."default",
-    "Apellidos" text COLLATE pg_catalog."default",
-    "Edad" bigint,
-    "Provincia en la que reside" text COLLATE pg_catalog."default",
-    "Telefono" text COLLATE pg_catalog."default",
-    "Indique si presenta alguna discapacidad" boolean,
-    "Número de la seguridad social" text COLLATE pg_catalog."default",
-    "¿Es usted soltero o viudo?" boolean,
-    "Indique si vive en una residencia de mayores" boolean,
-    "Indique si viajará con acompañante" boolean,
-    "Indique si el año pasado disfrutó de algún viaje" boolean,
-    "Indique si en 2021 disfrutó de algún viaje" boolean,
-    "Importe de su pensión percibida" bigint,
-    "Porcentaje de discapacidad" bigint
+    solicitud_id bigint PRIMARY KEY,
+    nombre varchar(50),
+    apellidos text COLLATE pg_catalog."default",
+    edad smallint,
+    provincia_residente varchar(50),
+    telefono numeric(9,0),
+    discapacidad boolean,
+    seguridad_social numeric(12,0),
+    soltero_o_viudo boolean,
+    vive_en_residencia boolean,
+    viajara_con_acompanante boolean,
+    imserso_anopasado boolean,
+    imserso_2021 boolean,
+    importe_pension smallint,
+    porcentaje_discapacidad tinyint,
+    CONSTRAINT solcitudes_pkey PRIMARY KEY (solicitud_id)
 );
 
-ALTER TABLE esquema."Solicitudes"
-    OWNER TO postgres;
 
--- Index: ix_esquema_Solicitudes_index
-
-CREATE INDEX "ix_esquema_Solicitudes_index"
-    ON esquema."Solicitudes" USING btree
-    (index ASC NULLS LAST)
-    TABLESPACE pg_default;
-
-
-
--- Table: esquema."DESTINOS"
-
--- DROP TABLE esquema."DESTINOS";
-
-CREATE TABLE esquema."DESTINOS"
+-- Table: esquema."destinos"
+CREATE TABLE esquema.destinos
 (
     index bigint,
-    "Destino" text COLLATE pg_catalog."default",
-    "Tipo de destino" bigint,
-    indice bigint
-)
+    destino text COLLATE pg_catalog."default",
+    tipo_destino tinyint,
+    CONSTRAINT destinos_pkey PRIMARY KEY (tipo_destino),
+    CONSTRAINT tdestino_fkey FOREIGN KEY (tipo_destino) REFERENCES esquema.tdestino(tipo_destino)
+);
 
-TABLESPACE pg_default;
-
-ALTER TABLE esquema."DESTINOS"
-    OWNER to postgres;
-
--- Index: ix_esquema_DESTINOS_index
-
--- DROP INDEX esquema."ix_esquema_DESTINOS_index";
-
-CREATE INDEX "ix_esquema_DESTINOS_index"
-    ON esquema."DESTINOS" USING btree
-    (index ASC NULLS LAST)
-    TABLESPACE pg_default;
-
-
--- Table: esquema."Preferencias"
-
--- DROP TABLE esquema."Preferencias";
-
-CREATE TABLE esquema."Preferencias"
+-- Table: esquema."preferencias"
+CREATE TABLE esquema.preferencias
 (
-    index bigint,
-    "índice" bigint,
-    "Opcion1" text COLLATE pg_catalog."default",
-    "Opcion2" text COLLATE pg_catalog."default",
-    "Opcion3" text COLLATE pg_catalog."default",
-    "Opcion4" text COLLATE pg_catalog."default",
-    "Opcion5" text COLLATE pg_catalog."default"
+    solicitud_id bigint PRIMARY KEY,
+    opcion_n tinyint,
+    ciudad text COLLATE pg_catalog "default",
+    CONSTRAINT preferencias_pkey PRIMARY KEY (solicitud_id)
+);
+
+-- Table: esquema."tipo de destino"
+CREATE TABLE esquema.tdestino
+(
+    tipo_destino tinyint PRIMARY KEY,
+    duracion text COLLATE pg_catalog."default",
+    CONSTRAINT tdestino_pkey PRIMARY KEY (tipo_destino)
+);
+
+
+-- Table: esquema."hoteles"
+CREATE TABLE esquema.hoteles
+(
+    hotel_id bigint PRIMARY KEY,
+    ciudad text COLLATE pg_catalog "default",
+    hotel text COLLATE pg_catalog "default"
 )
-
-TABLESPACE pg_default;
-
-ALTER TABLE esquema."Preferencias"
-    OWNER to postgres;
-
--- Index: ix_esquema_Preferencias_index
-
--- DROP INDEX esquema."ix_esquema_Preferencias_index";
-
-CREATE INDEX "ix_esquema_Preferencias_index"
-    ON esquema."Preferencias" USING btree
-    (index ASC NULLS LAST)
-    TABLESPACE pg_default;
-
