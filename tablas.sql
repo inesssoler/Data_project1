9f540baa93246ec6091b11be8db3b7746970df06
jarupu <javieruizpuchol@gmail.com>
2023-12-27 08:53:31 +0100

    primera conexion exitosa

--- tablas.sql
+++ tablas.sql
@@ -1,63 +1,54 @@
 -- Create esquema
-
 CREATE SCHEMA IF NOT EXISTS esquema;
 
-
 -- Table: esquema."Solicitudes"
-
-CREATE TABLE esquema.solicitudes
+CREATE TABLE esquema."solicitudes"
 (
     solicitud_id bigint PRIMARY KEY,
     nombre varchar(50),
-    apellidos text COLLATE pg_catalog."default",
+    apellidos text,
     edad smallint,
     provincia_residente varchar(50),
-    telefono numeric(9,0),
+    telefono varchar(50),
     discapacidad boolean,
-    seguridad_social numeric(12,0),
+    seguridad_social varchar(50),
     soltero_o_viudo boolean,
     vive_en_residencia boolean,
     viajara_con_acompanante boolean,
     imserso_anopasado boolean,
     imserso_2021 boolean,
     importe_pension smallint,
-    porcentaje_discapacidad tinyint,
-    CONSTRAINT solcitudes_pkey PRIMARY KEY (solicitud_id)
+    porcentaje_discapacidad smallint
 );
 
-
 -- Table: esquema."destinos"
-CREATE TABLE esquema.destinos
+CREATE TABLE esquema."destinos"
 (
     index bigint,
-    destino text COLLATE pg_catalog."default",
-    tipo_destino tinyint,
-    CONSTRAINT destinos_pkey PRIMARY KEY (tipo_destino),
-    CONSTRAINT tdestino_fkey FOREIGN KEY (tipo_destino) REFERENCES esquema.tdestino(tipo_destino)
+    destino text,
+    tipo_destino smallint,
+    CONSTRAINT destinos_pkey PRIMARY KEY (tipo_destino)
 );
 
 -- Table: esquema."preferencias"
-CREATE TABLE esquema.preferencias
+CREATE TABLE esquema."preferencias"
 (
     solicitud_id bigint PRIMARY KEY,
-    opcion_n tinyint,
-    ciudad text COLLATE pg_catalog "default",
-    CONSTRAINT preferencias_pkey PRIMARY KEY (solicitud_id)
+    opcion_n smallint,
+    ciudad text
 );
 
 -- Table: esquema."tipo de destino"
-CREATE TABLE esquema.tdestino
+CREATE TABLE esquema."tdestino"
 (
-    tipo_destino tinyint PRIMARY KEY,
-    duracion text COLLATE pg_catalog."default",
-    CONSTRAINT tdestino_pkey PRIMARY KEY (tipo_destino)
+    tipo_destino smallint PRIMARY KEY,
+    duracion text
 );
 
-
 -- Table: esquema."hoteles"
-CREATE TABLE esquema.hoteles
+CREATE TABLE esquema."hoteles"
 (
     hotel_id bigint PRIMARY KEY,
-    ciudad text COLLATE pg_catalog "default",
-    hotel text COLLATE pg_catalog "default"
-)
+    ciudad text,
+    hotel text
+);
